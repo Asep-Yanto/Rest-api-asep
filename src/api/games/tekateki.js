ae842cb12ed289698280a520f73737a7a6b467e9
Asep <wawang.yuli86@gmail.com>
2025-08-31 12:26:36 +0700

    Add files via upload

--- src/api/games/tekateki.js
+++ src/api/games/tekateki.js
@@ -1,1 +1,9 @@
+const tekaTeki = require("../../json/tekaTeki.json");
 
+module.exports = async (req, res) => {
+  const random = tekaTeki[Math.floor(Math.random() * tekaTeki.length)];
+  res.status(200).json({
+    success: true,
+    data: random
+  });
+};
